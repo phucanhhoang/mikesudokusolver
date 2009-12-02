8b47acbde1f33e67d7a55fd4a67e64965ecae866
mikeskirk <mikeskirk@9d301c24-dc85-11de-82ea-1f113a1b7651>
2009-12-02 20:05:43 +0000

    Attempting to solve without using strategies

---
+++ trunk/src/solver/parallelNoStrategies/ParallelSudokuPuzzle.java
@@ -0,0 +1,157 @@
+package solver.parallelNoStrategies;
+
+import gui.Constants;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import solver.SudokuBoard;
+import solver.SudokuPuzzle;
+import solver.parallel.solvers.ParallelBruteForceSolver;
+import solver.parallel.solvers.strategies.semaphore.CountingSemaphore;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ParallelSudokuPuzzle.
+ */
+public class ParallelSudokuPuzzle extends SudokuPuzzle {
+
+	/**
+	 * Instantiates a new parallel sudoku puzzle.
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
+	public ParallelSudokuPuzzle() throws InterruptedException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Instantiates a new parallel sudoku puzzle.
+	 * 
+	 * @param board
+	 *            the board
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
+	public ParallelSudokuPuzzle(int[][] board) throws InterruptedException {
+		super(board);
+	}
+
+	/**
+	 * Shutdown the inputted threadpool without risking problems.
+	 * 
+	 * @param threadPool
+	 *            the thread pool
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
+	private void shutdown(ThreadPoolExecutor threadPool)
+			throws InterruptedException {
+		// System.out.println("Shutting down threadPool");
+		threadPool.shutdown();
+		threadPool.awaitTermination(30, TimeUnit.SECONDS);
+		threadPool.shutdownNow();
+	}
+
+	/**
+	 * Solve.
+	 * 
+	 * @param strategy
+	 *            the strategy
+	 * 
+	 * @return true, if successful
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
+	@Override
+	public boolean solve() throws InterruptedException {
+		// Create the thread pool.
+		ThreadPoolExecutor threadPool;
+
+		/*
+		 * ParallelStrategySolver solver = new ParallelStrategySolver(board);
+		 * solver.solve(threadPool);
+		 */
+		
+		// If the board is solved then simply return out of this method.
+		if (!board.isSolved()) {
+			threadPool =(ThreadPoolExecutor) Executors
+				.newFixedThreadPool(Constants.NUM_BRANCHES);
+		} else {
+			return true;
+		}
+
+		/*
+		 * RecurssiveSudokuPuzzle bruteForce = new
+		 * RecurssiveSudokuPuzzle(board); bruteForce.solve();
+		 */
+
+		return runBruteForceSolver(threadPool);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see solver.SudokuPuzzle#solveBackwards()
+	 */
+	@Override
+	public boolean solveBackwards() throws InterruptedException {
+
+		return solve();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see solver.SudokuPuzzle#solveRandom()
+	 */
+	@Override
+	public boolean solveRandom() throws InterruptedException {
+
+		return solve();
+	}
+
+	/**
+	 * Starts the brute force solved.
+	 * 
+	 * @param threadPool
+	 * @return
+	 * @throws InterruptedException
+	 */
+	private boolean runBruteForceSolver(ThreadPoolExecutor threadPool)
+			throws InterruptedException {
+		// Initialize the counter that will monitor the number of threads created.
+		CountingSemaphore counter = new CountingSemaphore();
+		
+		// Create a new instance of the brute force solving class.
+		ParallelBruteForceSolver brute = new ParallelBruteForceSolver(board,
+				threadPool, counter, 0, 0);
+		ParallelBruteForceSolver.clearSolved();
+		brute.run();
+
+		// System.out.println("waiting for threads");
+		counter.waitForThreadsToFinish();
+
+		// brute.run();
+		shutdown(threadPool);
+
+		// System.out.println("FINAL COUNTER VALUE: " + counter.getValue());
+		
+		// If the board is solved it will be stored in a static variable in the solver.
+		SudokuBoard tempBoard = brute.getSolvedBoard();
+		if (tempBoard != null) {
+			board = tempBoard;
+			return true;
+		} else {
+			// System.out.println("NOT SOLVED");
+			return false;
+		}
+	}
+
+}
---
+++ trunk/src/solver/parallelNoStrategies/solvers/ParallelBruteForceNoStrategiesSolver.java
@@ -0,0 +1,250 @@
+package solver.parallelNoStrategies.solvers;
+
+import gui.Constants;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import solver.Helper;
+import solver.SudokuBoard;
+import solver.parallel.solvers.strategies.semaphore.CountingSemaphore;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ParallelBruteForceSolver.
+ */
+public class ParallelBruteForceNoStrategiesSolver implements Runnable {
+
+	/** The board. */
+	private SudokuBoard board;
+	
+	/** The solved board. */
+	private static SudokuBoard solvedBoard;
+	
+	/** The board solved. */
+	private static boolean boardSolved = false;
+	
+	/** The start row. */
+	private int startRow;
+	
+	/** The start col. */
+	private int startCol;
+	
+	/** The counter. */
+	private CountingSemaphore counter;
+	
+	/** The thread pool. */
+	ThreadPoolExecutor threadPool;
+	
+	/** The not been in run. */
+	private boolean notBeenInRun = false;
+
+	/**
+	 * Instantiates a new parallel brute force solver.
+	 */
+	public ParallelBruteForceNoStrategiesSolver() {
+		super();
+		counter = new CountingSemaphore();
+	}
+
+	/**
+	 * Instantiates a new parallel brute force solver.
+	 * 
+	 * @param board2 the board2
+	 * @param threadPool the thread pool
+	 * @param counter the counter
+	 * @param startRow the start row
+	 * @param startCol the start col
+	 */
+	public ParallelBruteForceNoStrategiesSolver(SudokuBoard board2,
+			ThreadPoolExecutor threadPool, CountingSemaphore counter,
+			int startRow, int startCol) {
+		// Initialize global variables.
+		board = board2;
+		this.threadPool = threadPool;
+		this.counter = counter;
+		this.startRow = startRow;
+		this.startCol = startCol;
+		counter.incrementCounter();
+		notBeenInRun = true;
+	}
+
+	/**
+	 * Clear solved.
+	 */
+	public synchronized static void clearSolved() {
+		boardSolved = false;
+		solvedBoard = null;
+	}
+
+	/**
+	 * Decrement counter if first layer of run.
+	 * 
+	 * @param firstLayerOfRun the first layer of run
+	 */
+	private void decrementCounterIfFirstLayerOfRun(boolean firstLayerOfRun) {
+		if (firstLayerOfRun) {
+			counter.decrementCounter();
+			firstLayerOfRun = false;
+		}
+	}
+
+	/**
+	 * Find next empty cell.
+	 * 
+	 * @return true, if successful
+	 */
+	private boolean findNextEmptyCell() {
+		int row = startRow;
+		int col = startCol;
+
+		while (board.getValueAt(row, col) != Constants.EMPTY_CELL) {
+			if (row == board.getSize() - 1) {
+				row = 0;
+
+				if (++col == board.getSize()) {
+					return false;
+				}
+			} else {
+				row++;
+			}
+		}
+		startRow = row;
+		startCol = col;
+		return true;
+	}
+
+	/**
+	 * Gets the solved board.
+	 * 
+	 * @return the solved board
+	 */
+	public SudokuBoard getSolvedBoard() {
+		if (boardSolved) {
+			return solvedBoard;
+		} else {
+			return null;
+		}
+	}
+
+	/** 
+	 * Only decrements counter on lowest layer of this method (It runs recurssively). 
+	 * Then it checks if the board is solved before running the strategies to solve
+	 * the board. If the strategies worked or there are no more empty cells it returns
+	 * failure. Then it runs the bruteForceSolving method to break up ths board into all
+	 * possible boards for the parallel solver to attempt again.
+	 * 
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		boolean firstLayerOfRun = false;
+
+		/*
+		 * If this is the first thim through this method in the
+		 */
+		if (notBeenInRun) {
+			firstLayerOfRun = true;
+			notBeenInRun = false;
+		}
+
+		if (ParallelBruteForceNoStrategiesSolver.boardSolved) {
+			decrementCounterIfFirstLayerOfRun(firstLayerOfRun);
+			return;
+		} else if (board.isSolved()) {
+			setSolvedBoard(board);
+			decrementCounterIfFirstLayerOfRun(firstLayerOfRun);
+			return;
+		}
+
+		threadPool =(ThreadPoolExecutor) Executors
+		.newFixedThreadPool(Constants.PROCS_PER_BRANCH);
+
+		if (board.isSolved() || boardSolved) {
+			setSolvedBoard(board);
+			decrementCounterIfFirstLayerOfRun(firstLayerOfRun);
+			return;
+		} else if (!findNextEmptyCell()) {
+			decrementCounterIfFirstLayerOfRun(firstLayerOfRun);
+			return;
+		}
+
+		startBruteForceSolver(threadPool);
+		decrementCounterIfFirstLayerOfRun(firstLayerOfRun);
+	}
+
+	/**
+	 * Sets the solved board.
+	 * 
+	 * @param board2 the new solved board
+	 */
+	private synchronized void setSolvedBoard(SudokuBoard board2) {
+		if (boardSolved) {
+			return;
+		}
+		System.out.println("BOARD SOLVED");
+		boardSolved = true;
+		solvedBoard = new SudokuBoard(Helper.copyCells(board2.getBoard()));
+	}
+
+	/**
+	 * Start brute force solver. Only uses the possible entries for the next possible
+	 * cell, traverses board breadth first if there are available spots for threads or
+	 * depth first if the threadPool is full.
+	 * 
+	 * @param threadPool the thread pool
+	 * @param solver the solver
+	 */
+	private void startBruteForceSolver(ThreadPoolExecutor threadPool) {
+		SudokuBoard tempBoard;
+		SudokuBoard boardSave;
+		int tempStartRow;
+		int tempStartCol;
+		for (int j=1; j <= board.getSize(); j++) {
+		 //System.out.println("Setting (" + startRow + " , " + startCol +
+		 //") to " + j);
+			if (board.legalMove(startRow, startCol, j)) {
+				tempBoard = board.newChangedValue(startRow, startCol, j);
+	
+				if (!startThread(tempBoard, j == board.getSize())) {
+					boardSave = board;
+					board = tempBoard;
+					tempStartRow = startRow;
+					tempStartCol = startCol;
+					run();
+					if (board.isSolved()) {
+						setSolvedBoard(board);
+					}
+					board = boardSave;
+					startRow = tempStartRow;
+					startCol = tempStartCol;
+					boardSave = null;
+					tempBoard = null;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Start thread.
+	 * 
+	 * @param tempBoard the temp board
+	 * 
+	 * @return true, if successful
+	 */
+	private synchronized boolean startThread(SudokuBoard tempBoard, boolean attempt) {
+
+		if (attempt && counter.getValue() < (Constants.NUM_BRANCHES)) {
+			ParallelBruteForceNoStrategiesSolver bruteSolver;
+			bruteSolver = new ParallelBruteForceNoStrategiesSolver(tempBoard, threadPool,
+					counter, startRow, startCol);
+			// new Thread(bruteSolver).start();
+			threadPool.execute(bruteSolver);
+			// bruteSolver.run();
+			return true;
+		}
+		return false;
+
+	}
+}
